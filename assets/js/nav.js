27c8e442d5b4ebb1f3fee78b5cf272fcffa07f93
jaubele <jaubele@ocde.us>
2026-09-02 13:47:58 -0700

    updated contact us options

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -32,7 +32,8 @@
     { key: 'about',                   label: 'About',                   isDropdown: true },
     { key: 'purpose',                 label: 'Purpose',                 file: 'purpose.html',                 parent: 'about' },
     { key: 'faq',                     label: 'FAQ',                     file: 'faq.html',                     parent: 'about' },
-    { key: 'contact',                 label: 'Contact Us',              href: 'https://form.asana.com/?k=FPztxN3MWCxoKTl6R6Ey7A&d=32468375920771', target: "_blank"},
+    { key: 'contact',                 label: 'Email Us',              href: 'mailto:redi@ocde.us', target: "_blank"},
+    { key: 'work',                 label: 'Work With Us',              href: 'https://form.asana.com/?k=FPztxN3MWCxoKTl6R6Ey7A&d=32468375920771', target: "_blank"}
   ];
 
   // ─── Path helper ─────────────────────────────────────────────────────────────
